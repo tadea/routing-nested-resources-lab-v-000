fc68dc2cb24b73d59c18ec79f990133eb59ddd39
Tadea Simunovic <tsimunoviczg@gmail.com>
2019-03-26 14:25:22 +0000

    Automatically backed up by Learn

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -8,7 +8,7 @@ describe "artists", type: :feature do
   end
 
   describe "GET /artists" do
-    it "links to artists songs path" do
+    #it "links to artists songs path" do
       visit artists_path
       expect(page).to have_link(@artist.name, href: artist_songs_path(@artist))
     end
